b4a49e41e2288e2fb7df7bf3e33fe428162fe57c
Camdyn Rasque <rasque.camdyn@gmail.com>
2021-12-01 15:51:40 -0800

    First day's challenges complete

---
+++ day-1/challenge-1.js
@@ -0,0 +1,21 @@
+// script.js
+
+const fs = require('fs');
+
+let input = fs.readFileSync('input.txt', 'utf-8').split('\n');
+input = input.map(str => Number(str));
+
+// For Testing:
+// const input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
+let prev = input[0];
+let numIncreasing = 0;
+for (let i = 1; i < input.length; i++) {
+  if (input[i] > prev) {
+    numIncreasing += 1;
+    console.log(`prev: ${prev}, curr: ${input[i]}, diff: ${input[i] - prev}`);
+  }
+  prev = input[i];
+}
+
+console.log(`\nFinal Count: ${numIncreasing}`);
---
+++ day-1/challenge-2.js
@@ -0,0 +1,26 @@
+// script.js
+
+const fs = require('fs');
+
+let input = fs.readFileSync('input.txt', 'utf-8').split('\n');
+input = input.map(str => Number(str));
+
+// For Testing:
+// const input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
+const reducer = (prev, curr) => prev + curr;
+const window = [input[0], input[1], input[2]];
+let prev = window.reduce(reducer);
+let numIncreasing = 0;
+for (let i = 3; i < input.length; i++) {
+  window.shift();
+  window.push(input[i]);
+  const curr = window.reduce(reducer)
+  if (curr > prev) {
+    numIncreasing += 1;
+    console.log(`prev: ${prev}, curr: ${curr}, diff: ${curr - prev}`);
+  }
+  prev = curr;
+}
+
+console.log(`\nFinal Count: ${numIncreasing}`);
